16787d7cd0224f5613672585851f4485d818cd0b
Doug Hellmann <doug@doughellmann.com>
2018-04-19 17:39:26 -0400

    optionally exec the conf.py file and pass settings to the template
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- sphinxcontrib/datatemplates/cli.py
+++ sphinxcontrib/datatemplates/cli.py
@@ -25,6 +25,10 @@ def _load_data(filename):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        '--config-file',
+        help='the path to conf.py',
+    )
+    parser.add_argument(
         'source',
         help='the path to the data file',
     )
@@ -36,6 +40,12 @@ def main():
 
     data = _load_data(args.source)
 
+    config_globals = {}
+    if args.config_file:
+        with io.open(args.config_file, 'r', encoding='utf-8') as f:
+            config_body = f.read()
+        exec(config_body, config_globals)
+
     with io.open(args.template, 'r', encoding='utf-8') as f:
         template_body = f.read()
 
@@ -44,5 +54,6 @@ def main():
         make_list_table=helpers.make_list_table,
         make_list_table_from_mappings=helpers.make_list_table_from_mappings,
         data=data,
+        **config_globals,
     )
     print(rendered)

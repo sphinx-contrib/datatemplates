9a27917f816ef5a5dfd63a71017aa4127f3c0375
Jan Brohl <janbrohl@t-online.de>
2019-06-13 18:35:59 +0200

    Set __version__

--- sphinxcontrib/datatemplates/__init__.py
+++ sphinxcontrib/datatemplates/__init__.py
@@ -1,8 +1,12 @@
+import pbr.version
 from sphinx.util import logging
 
 from . import directive, domain
 
 LOG = logging.getLogger(__name__)
+
+__version__ = pbr.version.VersionInfo(
+    "sphinxcontrib-datatemplates").version_string()
 
 
 def setup(app):
@@ -11,6 +15,7 @@ def setup(app):
     app.add_domain(domain.DataTemplateDomain)
 
     return {
+        'version': __version__,
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }

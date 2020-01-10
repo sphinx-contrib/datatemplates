c5bdc5b63ffd7b0d2cde271869542701531b1d16
Jan Brohl <janbrohl@t-online.de>
2020-01-10 20:38:30 +0100

    clarification

--- sphinxcontrib/datatemplates/cli.py
+++ sphinxcontrib/datatemplates/cli.py
@@ -9,20 +9,28 @@ from . import standalone
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--templates-folder',
-                        help='the path to the templates folder',
-                        default="./templates")
+                        help='path to the templates folder',
+                        default='./templates')
+
+    # Add subcommand for each datatemplate
     subparsers = parser.add_subparsers(title="subcommand", dest="cmd")
     for name, dt in standalone.datatemplate_names.items():
         p = subparsers.add_parser(name)
         for option_name in dt.option_spec.keys():
             p.add_argument("--" + option_name, default=argparse.SUPPRESS)
-    args = parser.parse_args()
-    options = vars(args).copy()
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
-        options.pop('templates_folder'), encoding='utf-8-sig'))
 
-    datatemplate = standalone.datatemplate_names[options.pop("cmd")](env,
-                                                                     options)
+    args = parser.parse_args()
+
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
+        args.templates_folder, encoding='utf-8-sig'))
+
+    # create options dict for datatemplate
+    options = vars(args).copy()
+    del options['templates_folder']
+    del options['cmd']
+
+    # do the actual work
+    datatemplate = standalone.datatemplate_names[args.cmd](env, options)
     print(datatemplate.run())
 
 

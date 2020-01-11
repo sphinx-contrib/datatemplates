55a93596f9302b32f3a10f7f91868854946e3a5c
Jan Brohl <janbrohl@t-online.de>
2020-01-11 14:22:48 +0100

    Move directive docs to code

--- sphinxcontrib/datatemplates/directive.py
+++ sphinxcontrib/datatemplates/directive.py
@@ -73,29 +73,117 @@ class DataTemplateDirective(rst.Directive):
         nested_parse_with_titles(self.state, result, node)
         return node.children
 
+    @classmethod
+    def usage(cls):
+        _, sep, doc = cls.__doc__.partition(".. rst:directive::")
+        return sep + doc
+
 
 class DataTemplateJSONDirective(mixins.DataTemplateJSON,
                                 DataTemplateDirective):
-    pass
+    """
+    .. rst:directive:: .. datatemplate:json:: source-path
+    
+        Load file at ``source-path`` (relative to the documentation build directory) via
+        :py:func:`json.load` and render using ``template`` given in directive body.
+
+        .. rst:directive:option:: template: template name, optional
+
+            The name of a template file on the Sphinx template search path. Overrides directive body.
+            
+        .. rst:directive:option:: encoding: optional, defaults to ``utf-8-sig``
+
+            The text encoding that will be used to read the source file. See :any:`standard-encodings`
+    """
 
 
 class DataTemplateCSVDirective(mixins.DataTemplateCSV, DataTemplateDirective):
-    pass
+    """
+    .. rst:directive:: .. datatemplate:csv:: source-path
+    
+        Load file at ``source-path`` (relative to the documentation build directory) via
+        :py:func:`csv.reader` or :py:class:`csv.DictReader` depending on ``header`` and render using ``template`` given in directive body.
+
+        .. rst:directive:option:: template: template name, optional
+
+                The name of a template file on the Sphinx template search path. Overrides directive body.
+                
+        .. rst:directive:option:: encoding: optional, defaults to ``utf-8-sig``
+
+                The text encoding that will be used to read the source file. See :any:`standard-encodings`
+
+        .. rst:directive:option:: header: flag, optional
+
+                Set to use :py:class:`csv.DictReader` for reading the file.
+                If not set :py:func:`csv.reader` is used.
+
+        .. rst:directive:option:: dialect: either ``auto`` or one from :py:func:`csv.list_dialects`, optional
+
+                Set to select a specific :py:class:`csv.Dialect`.
+                Set to ``auto``, to try autodetection.
+                If not set the default dialect is used.
+    """
 
 
 class DataTemplateYAMLDirective(mixins.DataTemplateYAML,
                                 DataTemplateDirective):
-    pass
+    """
+    .. rst:directive:: .. datatemplate:yaml:: source-path
+            
+        Load file at ``source-path`` (relative to the documentation build directory)  via 
+        PyYAML_ (:py:func:`yaml.safe_load`) and render using ``template`` given in directive body.
+
+        .. rst:directive:option:: template: template name, optional
+
+                The name of a template file on the Sphinx template search path. Overrides directive body.
+                
+        .. rst:directive:option:: encoding: optional, defaults to ``utf-8-sig``
+
+                The text encoding that will be used to read the source file. See :any:`standard-encodings`
+
+        .. rst:directive:option:: multiple-documents: flag, optional
+
+                Set to read multiple documents from the file into a :py:class:`list`
+
+    .. _PyYAML: https://pyyaml.org
+    """
 
 
 class DataTemplateXMLDirective(mixins.DataTemplateXML, DataTemplateDirective):
-    pass
+    """
+    .. rst:directive:: .. datatemplate:xml:: source-path
+    
+        Load file at ``source-path`` (relative to the documentation build directory)  via
+        :py:func:`xml.etree.ElementTree.parse` (actually using ``defusedxml``) and render using ``template`` given in directive body.
+
+        .. rst:directive:option:: template: template name, optional
+
+                The name of a template file on the Sphinx template search path. Overrides directive body.
+    """
 
 
 class DataTemplateDBMDirective(mixins.DataTemplateDBM, DataTemplateDirective):
-    pass
+    """
+    .. rst:directive:: datatemplate:dbm:: source-path
+    
+        Load DB at ``source-path`` (relative to the documentation build directory)  via
+        :py:func:`dbm.open` and render using ``template`` given in directive body.
+
+        .. rst:directive:option:: template: template name, optional
+
+                The name of a template file on the Sphinx template search path. Overrides directive body.
+    """
 
 
 class DataTemplateImportModuleDirective(mixins.DataTemplateImportModule,
                                         DataTemplateDirective):
-    pass
+    """
+    .. rst:directive:: .. datatemplate:import-module:: module-name
+        
+        Load module ``module-name`` (must be importable in ``conf.py``)  via 
+        :py:func:`importlib.import_module` and render using ``template`` given in directive body.
+
+        .. rst:directive:option:: template: template name, optional
+
+                The name of a template file on the Sphinx template search path. Overrides directive body.
+    """

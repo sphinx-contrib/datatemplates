8e84d32021079df45df3e27da0ec134f4f208ddf
Jan Brohl <janbrohl@t-online.de>
2020-01-18 21:59:22 +0100

    add loaders registry

--- sphinxcontrib/datatemplates/loaders.py
+++ sphinxcontrib/datatemplates/loaders.py
@@ -5,6 +5,34 @@ import defusedxml.ElementTree as ET
 import dbm
 import importlib
 import csv
+import re
+import mimetypes
+
+registered_loaders = []
+
+
+class LoaderEntry:
+    def __init__(self, loader, name, match_source):
+        self.loader = loader
+        self.name = name
+        if isinstance(match_source, str):
+            self.match_source = re.compile(match_source).match
+        else:
+            self.match_source = match_source
+
+
+def loader_for_source(source, default=None):
+    for e in registered_loaders:
+        if e.match_source(source):
+            return e.loader
+    return default
+
+
+def loader_by_name(name, default=None):
+    for e in registered_loaders:
+        if e.name == name:
+            return e.loader
+    return default
 
 
 @contextlib.contextmanager
@@ -33,10 +61,16 @@ def load_csv(source,
         yield list(r)
 
 
+registered_loaders.append(LoaderEntry(load_csv, "csv", ".*?[.]csv$"))
+
+
 @contextlib.contextmanager
 def load_json(source, absolute_resolved_path, encoding='utf-8-sig', **options):
     with open(absolute_resolved_path, 'r', encoding=encoding) as f:
         yield json.load(f)
+
+
+registered_loaders.append(LoaderEntry(load_json, "json", ".*?[.]json$"))
 
 
 @contextlib.contextmanager
@@ -54,15 +88,30 @@ def load_yaml(source,
             yield yaml.safe_load(f)
 
 
+registered_loaders.append(LoaderEntry(load_yaml, "yaml", ".*?[.]yaml$"))
+
+
 @contextlib.contextmanager
 def load_xml(source, absolute_resolved_path, **options):
     yield ET.parse(absolute_resolved_path).getroot()
+
+
+registered_loaders.append(
+    LoaderEntry(load_xml, "xml",
+                (lambda source: "xml" in mimetypes.guess_type(source)[0])))
 
 
 def load_dbm(source, absolute_resolved_path, **options):
     return dbm.open(absolute_resolved_path, "r")
 
 
+registered_loaders.append(LoaderEntry(load_dbm, "dbm", None))
+
+
 @contextlib.contextmanager
 def load_import_module(source, **options):
     yield importlib.import_module(source)
+
+
+registered_loaders.append(
+    LoaderEntry(load_import_module, "import-module", None))

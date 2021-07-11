8849dfca7f67c01772126fd330f60360bf20e98b
Manuel Racle <manuel.racle@gmail.com>
2021-07-12 00:08:06 +0200

    Added a resolve_any_xref() method to DataTemplateDomain

--- sphinxcontrib/datatemplates/domain.py
+++ sphinxcontrib/datatemplates/domain.py
@@ -19,9 +19,11 @@ class DataTemplateDomain(Domain):
     def get_objects(self):
         return []
 
-    def resolve_xref(self, env, fromdocname, builder, typ, target, node,
-                     contnode):
+    def resolve_xref(self, env, fromdocname, builder, typ, target, node, contnode):
         return None
+    
+    def resolve_any_xref(self, env, fromdocname, builder, target, node, contnode):
+        return []
 
     def merge_domaindata(self, docnames, otherdata):
         pass

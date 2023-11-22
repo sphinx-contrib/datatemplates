d8f8819b5e6c57450156ffbb974e2c27a6159768
Bizordec <kvabart@gmail.com>
2023-11-22 22:38:30 +0300

    fix compatibility with older python versions in tests

--- tests/custom_template_bridge/lib/template_bridge.py
+++ tests/custom_template_bridge/lib/template_bridge.py
@@ -1,6 +1,14 @@
-from sphinx.builders import Builder
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from sphinx.jinja2glue import BuiltinTemplateLoader
-from sphinx.theming import Theme
+
+if TYPE_CHECKING:
+    from typing import List, Optional
+
+    from sphinx.builders import Builder
+    from sphinx.theming import Theme
 
 
 def upperstring(input: str):
@@ -12,8 +20,8 @@ class CustomTemplateLoader(BuiltinTemplateLoader):
     def init(
         self,
         builder: Builder,
-        theme: Theme | None = None,
-        dirs: list[str] | None = None,
+        theme: Optional[Theme] = None,
+        dirs: Optional[List[str]] = None,
     ) -> None:
         super().init(builder, theme, dirs)
         self.environment.filters["upperstring"] = upperstring
--- tests/test_custom_template_bridge.py
+++ tests/test_custom_template_bridge.py
@@ -7,10 +7,18 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from bs4 import BeautifulSoup
+from sphinx import version_info as sphinx_version_info
 from sphinx.testing.util import SphinxTestApp
 
 if TYPE_CHECKING:
     from typing import Callable
+
+if sphinx_version_info >= (7, 2):
+    _path = Path
+else:
+    from sphinx.testing.path import path
+
+    _path = path
 
 
 def test_custom_template_bridge(
@@ -20,7 +28,7 @@ def test_custom_template_bridge(
     dataset_path = Path(os.path.dirname(__file__), "custom_template_bridge")
     shutil.copytree(dataset_path, tmp_path, dirs_exist_ok=True)
 
-    app = make_app(srcdir=tmp_path, warning=sys.stdout)
+    app = make_app(srcdir=_path(tmp_path), warning=sys.stdout)
     app.build()
 
     assert app._warncount == 0, "\n".join(app.messagelog)
